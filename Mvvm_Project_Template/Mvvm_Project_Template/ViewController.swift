64160ee03a4a6dc4c75dba5c995c9b9b449623c8
Osman YESIL <osmanyesil@outlook.com>
2022-12-20 15:29:40 +0300

    - Initial commit

---
+++ Mvvm_Project_Template/Mvvm_Project_Template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mvvm_Project_Template
+//
+//  Created by Osman Yesil on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
